1a8dd81fc8319ee8dd20b823865a77b1df98ef6f
Garrett Van Dyke <gvd417@gmail.com>
2019-09-09 00:32:45 -0500

    Added pics and roll counter

--- GUI.py
+++ GUI.py
@@ -1,10 +1,11 @@
 import tkinter as tk
 import PassThePigs as ptp
 import time
+from PIL import ImageTk, Image
 
 def showGUI():
 
-    LARGE_FONT= ("Verdana", 12)
+    LARGE_FONT = ("Verdana", 24, 'bold')
 
     class GUI(tk.Tk):
 
@@ -24,6 +25,7 @@ def showGUI():
             frame.grid(row=0, column=0, sticky="nsew")
 
             self.show_frame(GamePage)
+            self.title('Pass the Pigs')
         
         def show_frame(self, cont):
 
@@ -34,25 +36,48 @@ def showGUI():
 
         def __init__(self, parent, controller):
             tk.Frame.__init__(self, parent)
-            label = tk.Label(self, text="Pass the Pigs", font=LARGE_FONT)
+            label = tk.Label(self, text='Pass the Pigs', font=LARGE_FONT)
             label.grid(row=0, columnspan=2, pady=10, padx=10)
 
             rollButton = tk.Button(self, text='Roll', command=self.rolling, bg='blue')
             rollButton.config(height=2, width=15, activebackground='blue')
             rollButton.grid(row=1, columnspan=2)
+
+            self.numRolls = 1
         
         def rolling(self):
             resultList = ptp.passThePigs()
             
             text1 = tk.Text(self, height=2, width=30)
             text2 = tk.Text(self, height=2, width=30)
-            
-            text1.grid(row=3, column=0)
-            text2.grid(row=3, column=1)
-            
-            text1.insert(tk.END, resultList[0])
-            text2.insert(tk.END, resultList[1])
+            rollWidget = tk.Text(self, height=2, width=30)
 
+            text1.grid(row=4, column=0)
+            text2.grid(row=4, column=1)
+            rollWidget.grid(row=3, columnspan=2)
+
+            text1.tag_configure('center', justify='center')
+            text2.tag_configure('center', justify='center')
+            rollWidget.tag_configure('center', justify='center')
+
+            text1.insert(tk.END, resultList[0], 'center')
+            text2.insert(tk.END, resultList[1], 'center')
+            rollWidget.insert(tk.END, 'Roll ' + str(self.numRolls), 'center')
+
+            imageCanvas1 = tk.Canvas(self, width = 70, height=100)
+            imageCanvas2 = tk.Canvas(self, width = 70, height=100)
+
+            imageCanvas1.grid(row=5, column=0)
+            imageCanvas2.grid(row=5, column=1)
+
+            self.image1 = ImageTk.PhotoImage(Image.open(resultList[0].lower().replace(' ','') + '.gif'))
+            imageCanvas1.create_image(20, 20, anchor='nw', image=self.image1)
+
+            self.image2 = ImageTk.PhotoImage(Image.open(resultList[1].lower().replace(' ','') + '.gif'))
+            imageCanvas2.create_image(20, 20, anchor='nw', image=self.image2)
+
+            self.numRolls += 1
+            
     app = GUI()
     app.mainloop()
 
--- PassThePigs.py
+++ PassThePigs.py
@@ -1,12 +1,7 @@
 import random
-from PIL import Image
 
 def passThePigs():
-    # keep rolling while true
-    #game = True
-
-    #while game:
-        # get two numbers 1-100 inclusive
+    # two random numbers 1-100 inclusive
     pigList = [random.randint(1,100), random.randint(1,100)]
     resultList = []
 
@@ -23,26 +18,4 @@ def passThePigs():
         elif i == 100:
             resultList.append('Leaning Jowler')
     
-    # print results, prompt next move
-    #print('\n' + resultList[0] + ' ' + resultList[1])
-    # this image idea was not great
-    #showImage(resultList[0], resultList[1])
-    #userStopGo = input('Press enter to roll again, or type "q" to quit: ')
-
-    # quit scenario
-    #if userStopGo == 'q':
-     #   game = False
-      #  print('\nDrive safe! (a.k.a. get an Uber!)')
     return resultList
-def showImage(pig1, pig2):
-    # format filename
-    pig1 = pig1.lower().replace(' ','')
-    pig2 = pig2.lower().replace(' ','')
-
-    pig1Pic = Image.open(pig1 + '.gif')
-    pig2Pic = Image.open(pig2 + '.gif')
-
-    pig1Pic.show()
-    pig2Pic.show()
-
-passThePigs()
